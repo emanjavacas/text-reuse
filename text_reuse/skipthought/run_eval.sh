11f738583650c87a83379f1ce0ef7d2e0b0e7b96
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2018-04-19 15:24:05 +0200

    rename

--- text_reuse/skipthought/run_eval.sh
+++ text_reuse/skipthought/run_eval.sh
@@ -2,7 +2,7 @@
 for path in "$@"; do
     echo $path
     for model in $path/*.pt; do
-	python text_reuse/skipthought/sent_eval.py --model $model --gpu
+	python text_reuse/skipthought/run_senteval.py --model $model --gpu
     done
 done
 
