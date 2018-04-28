be956d52900e667197fa397223c859a2232809bb
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2018-04-28 17:29:09 +0200

    fix test

--- text_reuse/skipthought/test_dataiter.py
+++ text_reuse/skipthought/test_dataiter.py
@@ -3,15 +3,18 @@ import seqmod
 from dataiter import lines, DataIter
 
 fname = '/tmp/test.txt'
-with open(fname, 'w') as f:
+with open(fname, 'w+') as f:
     for i in range(10000):
-        f.write(str(i))
+        f.write(str(i) + '\n')
 
 d=seqmod.misc.Dict().fit(lines(fname))
-data=DataIter(d, fname, min_len=1)
-batches=data.batch_generator(10)
+batches=DataIter(d, fname, min_len=1).batch_generator(10, buffer_size=1000)
+asserts = 0
 for (src, _), ((trg1, _), (trg2, _)) in batches:
     for i in range(src.size(1)):
         for s, t1, t2 in zip(src[:, i].data, trg1[:, i].data, trg2[:, i].data):
             s, t1, t2 = int(d.vocab[s]), int(d.vocab[t1]), int(d.vocab[t2])
             assert s == t1 + 1, s == t2 - 1
+            asserts += 1
+
+print("Asserts:", asserts)
