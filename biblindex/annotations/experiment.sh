9e07bfaedd74b480eebaef438e5be61e8b990098
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2019-01-21 16:00:33 +0100

    missing

--- biblindex/annotations/experiment.sh
+++ biblindex/annotations/experiment.sh
@@ -2,7 +2,12 @@
 N=(1000 5000 10000 20000 35000);
 for n in ${N[@]}; do
     # Retrieval with word embeddings
-    python retrieval.py --n_background $n --lemma; python retrieval.py --n_background $n;
+    python retrieval.py --n_background $n --lemma
+    python retrieval.py --n_background $n
+    # Retrieval with word embeddings on non-lexical matches at 2
+    python retrieval.py --avoid_lexical --n_background $n --lemma
+    python retrieval.py --avoid_lexical --n_background $n
     # Tesserae
-    python baseline.py --n_background $n --lemma; python baseline.py --n_background $n;
+    python baseline.py --n_background $n --lemma
+    python baseline.py --n_background $n
 done
