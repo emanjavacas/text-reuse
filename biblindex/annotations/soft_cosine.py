63b21217e37d7fda2aecdc096e8f02a06d080d13
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2019-02-06 14:39:03 +0100

    added some docstring

--- biblindex/annotations/soft_cosine.py
+++ biblindex/annotations/soft_cosine.py
@@ -78,6 +78,13 @@ def soft_cosine2(src, trg, vocab, S):
 
 # speed up by caching computations
 def soft_cosine4(ss, ts, M):
+    """
+    ss : n query docs in BOW format, np.array(n, vocab)
+    ts : m indexed docs in BOW format, np.array(m, vocab)
+    M : similarity matrix, np.array(vocab, vocab)
+
+    returns : sims, soft cosine similarities, np.array(n, m)
+    """
     sims = np.zeros((len(ss), len(ts)))
     MtsT = M @ ts.T
     den2 = np.sqrt(np.diag(ts @ MtsT))
@@ -89,6 +96,20 @@ def soft_cosine4(ss, ts, M):
 
 
 def get_M(S, vocab, factor=1):
+    """
+    Transform an input similarity matrix for a possibly reduced vocabulary space
+    into the similarity matrix for the whole space (i.e. include OOVs). It assumes
+    that OOVs are indexed at the end of the space - e.g. for a vocab of 1001 where
+    the word "aardvark" doesn't have an entry in S (1000 x 1000), the entry for
+    "aardvark" is 1001. By default the similarity vector for OOVs is a one-hot vector
+    implying that the word is only similar to itself.
+
+    S : input similarity matrix (e.g. sklearn.metrics.pairwise.cosine_similarity(W) 
+        where W is your embedding matrix), np.array(vocab, vocab)
+    vocab : list of all words in your space
+    factor : raise your similarities to this power to reduce model confidence on word
+        similarities
+    """
     M = np.zeros([len(vocab), len(vocab)])
     M[:len(S), :len(S)] = np.power(np.clip(S, a_min=0, a_max=np.max(S)), factor)
     np.fill_diagonal(M, 1)
