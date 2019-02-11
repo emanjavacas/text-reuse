f82828ef7d681cd7576618cc1a8b36463873627f
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2019-02-11 13:37:50 +0100

    rename

---
+++ biblindex/annotations/create_bernard_background.py
@@ -0,0 +1,67 @@
+
+import os
+import difflib
+from lxml import etree
+import utils
+
+
+def LCS(a, b):
+    return difflib.SequenceMatcher(None, a, b).find_longest_match(0, len(a), 0, len(b))
+
+
+def get_doc_id(tree):
+    return tree.find('//div1').attrib['n'] + '-' + tree.find('//div2').attrib['n']
+
+
+def get_verses(tree):
+    elems = tree.xpath('//*[local-name() = "s" or local-name() = "milestone"]')
+    for idx, (milestone, s) in enumerate(zip(elems[::2], elems[1::2])):
+        assert milestone.tag == 'milestone', milestone.tag
+        assert s.tag == 's', s.tag
+        assert idx + 1 == int(milestone.attrib['n']), (idx + 1, milestone.attrib['n'])
+        yield idx + 1, s.text
+
+
+if __name__ == '__main__':
+    import pie
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device', default='cpu')
+    args = parser.parse_args()
+
+    model = pie.SimpleModel.load("./capitula.model.tar")
+    model.to(args.device)
+    bible = list(utils.load_bible().values())
+    texts = []
+    with open('background.bible.csv', 'w') as outf:
+        for f in os.listdir('./NT'):
+            if not f.endswith('xml'):
+                continue
+            print(f)
+
+            with open(os.path.join('NT', f)) as fn:
+                tree = etree.fromstring(fn.read().encode('utf-8')).getroottree()
+                doc_id = get_doc_id(tree)
+
+                try:
+                    for idx, s in get_verses(tree):
+                        text = s.replace('\n', ' ')
+                        is_dup = False
+                        for ref in bible:
+                            ref = ref['text']
+                            if LCS(text.split(), ref.split()).size / \
+                               len(text.split()) > 0.9:
+                                print("Dup!", f, idx)
+                                print(text)
+                                print(ref)
+                                print()
+                                is_dup = True
+                                break
+                        if not is_dup and text:
+                            lemma = utils.lemmatize(
+                                model, text.lower().split(), device=args.device
+                            )['lemma']
+                            row = '\t'.join([doc_id+'-'+str(idx), text, ' '.join(lemma)])
+                            outf.write(row + '\n')
+                except Exception as e:
+                    print('ERROR:', e)
---
+++ biblindex/annotations/create_bernard_gold.py
@@ -0,0 +1,109 @@
+
+import json
+import os
+import statistics
+
+import extract
+import utils
+
+# source data
+def select(*docs):
+    # select the one closest to the median/mean
+    lens = [(len(doc['selectedText'].split()), idx) for idx, doc in enumerate(docs)]
+    median = statistics.median([l for l, _ in lens])
+    idx = next(iter(sorted([(abs(l - median), idx) for l, idx in lens])))[1]
+    return docs[idx]
+
+
+def load_notes(user):
+    fpath = "{}.notes.csv".format(user)
+    if os.path.isfile(fpath):
+        with open(fpath) as f:
+            return set([line.split(',')[0] for line in f])
+    return set()
+
+
+def load_docs():
+    by_user = {user: extract.load_annotations(user) for user in extract.DATASETS}
+    shared = extract.get_shared(by_user)
+
+    # remove controversial as per user notes
+    for user in by_user:
+        notes = load_notes(user)
+        by_user[user] = {i: doc for i, doc in by_user[user].items() if i not in notes}
+    docs = []
+
+    for i in shared:
+        docs.append(
+            select(*[by_user[user][i] for user in by_user if i in by_user[user]]))
+
+    for user in by_user:
+        for idx, doc in by_user[user].items():
+            if idx in shared:
+                continue
+
+            docs.append(doc)
+
+    return docs
+
+
+def load_source(path='../splits/SCT1-5.json'):
+    with open(path) as f:
+        source = {}
+        for line in f:
+            doc = json.loads(line)
+            source[doc['id']] = doc
+
+    return source
+
+
+def enrich_with_lemmas_from_source(docs):
+    source = load_source()
+
+    for doc in docs:
+        sdoc = source[doc['id']]
+        text = [w['lemma'] for w in sdoc['textdata']]
+        text = [w['lemma'] for w in sdoc['textcontext']['prev'][-doc['prevSpan']:]] + text
+        text += [w['lemma'] for w in sdoc['textcontext']['next'][:doc['nextSpan']]]
+        doc['text_lemma'] = ' '.join(text)
+        assert len(doc['text_lemma'].split()) == len(doc['selectedText'].split())
+
+    return docs
+
+
+if __name__ == '__main__':
+    import pie
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--window', type=int, default=10)
+    parser.add_argument('--lemma_model', default="./capitula.model.tar")
+    args = parser.parse_args()
+
+    model = pie.SimpleModel.load(args.lemma_model)
+    docs, bible = load_docs(), utils.load_bible()
+    docs = enrich_with_lemmas_from_source(docs)
+    source = load_source()
+
+    gold, window = "bernard-gold.csv", "bernard-gold-window-{}.csv".format(args.window)
+    with open(gold, "w") as f, open(window, "w") as f2:
+        for doc in docs:
+            src, trg = doc['selectedText'], bible[doc['id']]
+            src_id, trg, trg_id = doc['id'], trg['text'], trg['url']
+            if len(src.split()) > 40:  # ignore longer than 40 words
+                continue
+            src_lemma = doc['text_lemma']
+            trg_lemma = ' '.join(utils.lemmatize(model, trg.lower().split())['lemma'])
+            f.write('\t'.join([src_id, trg_id, src, trg, src_lemma, trg_lemma]) + '\n')
+
+            # window
+            sdoc = source[doc['id']]
+            src = [w['word'] for w in sdoc['textdata']]
+            src = [w['word'] for w in sdoc['textcontext']['prev'][-args.window:]] + src
+            src += [w['word'] for w in sdoc['textcontext']['next'][:args.window]]
+            src = ' '.join(src)
+            src_lemma = [w['lemma'] for w in sdoc['textdata']]
+            src_lemma = [w['lemma'] for w in sdoc['textcontext']['prev'][-args.window:]]\
+                        + src_lemma
+            src_lemma += [w['lemma'] for w in sdoc['textcontext']['next'][:args.window]]
+            src_lemma = ' '.join(src_lemma)
+            f2.write('\t'.join([src_id, trg_id, src, trg, src_lemma, trg_lemma]) + '\n')
