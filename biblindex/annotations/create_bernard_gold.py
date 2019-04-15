d4f7f08654978acf870b30a045288659a7f38f5a
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2019-04-15 18:46:21 +0200

    added sentence tokenized

--- biblindex/annotations/create_bernard_gold.py
+++ biblindex/annotations/create_bernard_gold.py
@@ -3,6 +3,13 @@ import json
 import os
 import statistics
 
+from cltk.tokenize.latin.sentence import SentenceTokenizer
+try:
+    tok = SentenceTokenizer()
+except:
+    print("Couldn't find cltk sentence tokenizer")
+    tok = None
+
 import extract
 import utils
 
@@ -71,6 +78,49 @@ def enrich_with_lemmas_from_source(docs):
     return docs
 
 
+def window_sentence(sdoc, window):
+    src = [w['word'] for w in sdoc['textcontext']['prev'][-window:]] + \
+          [w['word'] for w in sdoc['textdata']] + \
+          [w['word'] for w in sdoc['textcontext']['next'][:window]]
+    src = ' '.join(src)
+    src_lemma = [w['lemma'] for w in sdoc['textcontext']['prev'][-window:]] + \
+                [w['lemma'] for w in sdoc['textdata']] + \
+                [w['lemma'] for w in sdoc['textcontext']['next'][:window]]
+    src_lemma = ' '.join(src_lemma)
+
+    return src, src_lemma
+
+
+def sentence_tokenize(sdoc, tok):
+    start = len(sdoc['textcontext']['prev'])  # index of 1st in
+    end = start + len(sdoc['textdata'])  # index of 1st outside
+    sents = tok.tokenize(' '.join(
+        [w['word'] for w in sdoc['textcontext']['prev']] +
+        [w['word'] for w in sdoc['textdata']] +
+        [w['word'] for w in sdoc['textcontext']['next']]))
+    sums = [0]
+    for s in sents:
+        sums.append(sums[-1] + len(s.split()))
+    s_start = s_end = None
+    for idx, s in enumerate(sums):
+        if s >= start and s_start is None:
+            s_start = idx - 1
+        if s >= end:
+            s_end = idx
+            break
+    
+    src = ' '.join(sents[s_start:s_end])
+    src_lemma = [w['lemma'] for w in sdoc['textcontext']['prev']] + \
+                [w['lemma'] for w in sdoc['textdata']] + \
+                [w['lemma'] for w in sdoc['textcontext']['next']]
+
+    src_lemma = ' '.join(src_lemma[sums[s_start]: sums[s_end]])
+
+    assert len(src.split()) == len(src_lemma.split())
+
+    return src, src_lemma
+
+
 if __name__ == '__main__':
     import pie
     import argparse
@@ -85,7 +135,8 @@ if __name__ == '__main__':
     source = load_source()
 
     gold, window = "bernard-gold.csv", "bernard-gold-window-{}.csv".format(args.window)
-    with open(gold, "w") as f, open(window, "w") as f2:
+    sentence = "bernard-gold-sentence.csv"
+    with open(gold, "w") as f, open(window, "w") as f2, open(sentence, "w") as f3:
         for doc in docs:
             src, trg = doc['selectedText'], bible[doc['id']]
             src_id, trg, trg_id = doc['id'], trg['text'], trg['url']
@@ -96,14 +147,12 @@ if __name__ == '__main__':
             f.write('\t'.join([src_id, trg_id, src, trg, src_lemma, trg_lemma]) + '\n')
 
             # window
-            sdoc = source[doc['id']]
-            src = [w['word'] for w in sdoc['textdata']]
-            src = [w['word'] for w in sdoc['textcontext']['prev'][-args.window:]] + src
-            src += [w['word'] for w in sdoc['textcontext']['next'][:args.window]]
-            src = ' '.join(src)
-            src_lemma = [w['lemma'] for w in sdoc['textdata']]
-            src_lemma = [w['lemma'] for w in sdoc['textcontext']['prev'][-args.window:]]\
-                        + src_lemma
-            src_lemma += [w['lemma'] for w in sdoc['textcontext']['next'][:args.window]]
-            src_lemma = ' '.join(src_lemma)
+            src, src_lemma = window_sentence(source[doc['id']], args.window)
             f2.write('\t'.join([src_id, trg_id, src, trg, src_lemma, trg_lemma]) + '\n')
+
+            # sentence
+            if tok is None:
+                print("Not compiling sentence tokenized version")
+                continue
+            src, src_lemma = sentence_tokenize(source[doc['id']], tok)
+            f3.write('\t'.join([src_id, trg_id, src, trg, src_lemma, trg_lemma]) + '\n')
